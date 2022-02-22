c5052b1c17a6c0a2334bd1f0645074f5e0326936
luketeeeeee <henrique.lucas521@gmail.com>
2022-02-22 17:03:20 -0300

    setted the function to add new tasks

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,65 @@
-import { TaskInput } from "../TaskInput";
-import { TaskList } from "../TaskList";
+import { FormEvent, useState } from "react";
+import addImg from "../../assets/add.svg";
+import deleteImg from "../../assets/delete.svg";
 import { Container } from "./styles";
 
+type Task = {
+  id: number;
+  name: string;
+  isComplete: boolean;
+}
+
 export function Main() {
+  const [title, setTitle] = useState('');
+  const [taskId, setTaskId] = useState(0);
+  const [isTaskCompleted, setIsTaskCompleted] = useState(false);
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    setTitle('');
+    setTaskId(taskId + 1);
+    setIsTaskCompleted(isTaskCompleted);
+
+    setTasks([...tasks, { id: taskId, name: title, isComplete: isTaskCompleted }]);
+  }
+
   return (
     <Container>
-      <TaskInput />
-      <TaskList />
+      <form onSubmit={handleCreateNewTask} autoComplete="off">
+        <h2>minhas tasks</h2>
+
+        <div>
+          <input
+            type="text"
+            id="task-title-input"
+            placeholder="nome da task"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+          <button type="submit" >
+            <img src={addImg} alt="Adicionar task" />
+          </button>
+        </div>
+      </form>
+
+      <main>
+        {tasks.map(task => (
+          <>
+            <div className="new-task" key={task.id}>
+              <div className="info-group">
+                <input type="checkbox" className="task-checkbox" />
+                <p>{task.name}</p>
+              </div>
+              <button>
+                <img src={deleteImg} alt="Deletar task" />
+              </button>
+            </div>
+            <hr />
+          </>
+        ))}
+      </main>
     </Container>
   );
 }
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,8 +1,75 @@
 import styled from "styled-components";
 
-export const Container = styled.main`
-  margin: -6rem auto 0;
+export const Container = styled.div`
   max-width: 800px;
+  margin: -6rem auto 0;
+  padding: 3rem 4rem;
   background: white;
   border-radius: 0.75rem;
+
+  form {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 3rem;
+
+    h2 {
+      font-weight: 600;
+      font-size: 1.7rem;
+    }
+
+    div {
+      display: flex;
+
+      #task-title-input {
+        height: 30px;
+        width: 220px;
+        margin-right: 0.2rem;
+        padding: 1rem 0.3rem 1rem 0.7rem;
+        border: none;
+        border-radius: 0.25rem;
+        background-color: lightgray;
+      }
+
+      button {
+        width: 32px;
+        border: none;
+        border-radius: 0.35rem;
+        background-color: var(--green);
+      }
+    }
+  }
+
+  main {
+    .new-task {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0.8rem 0;
+
+      .info-group {
+        display: flex;
+        align-items: center;
+
+        input {
+          height: 20px;
+          width: 20px;
+          margin-right: 0.5rem;
+        }
+
+        input:hover {
+          cursor: pointer;
+        }
+      }
+
+      button {
+        border: none;
+        background: none;
+      }
+    }
+
+    hr {
+      border: 1px solid lightgray;
+    }
+  }
 `;
