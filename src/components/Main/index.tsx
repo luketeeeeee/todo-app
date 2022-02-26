d7b674253498428b3860f4dd6d62f57f69c45798
luketeeeeee <henrique.lucas521@gmail.com>
2022-02-26 01:08:30 -0300

    added the following features: delete task, change task isCompleted property when the input checkbox change and do not allow the user to create a new task without the name

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,11 @@
 import { FormEvent, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+import { Container } from "./styles";
+import 'react-toastify/dist/ReactToastify.css';
+
 import addImg from "../../assets/add.svg";
 import deleteImg from "../../assets/delete.svg";
-import { Container } from "./styles";
 
 type Task = {
   id: number;
@@ -18,11 +22,31 @@ export function Main() {
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault();
 
+    if (/^\s*$/.test(title)) {
+      toast.info("Dê um nome à tarefa!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+
+      return;
+    }
+
     setTitle('');
     setTaskId(taskId + 1);
     setIsTaskCompleted(isTaskCompleted);
 
     setTasks([...tasks, { id: taskId, name: title, isComplete: isTaskCompleted }]);
+  }
+
+  function handleRemoveTask(taskId: number) {
+    const newTaskList = tasks.filter((task) => task.id !== taskId);
+
+    setTasks(newTaskList);
   }
 
   return (
@@ -46,20 +70,22 @@ export function Main() {
 
       <main>
         {tasks.map(task => (
-          <>
-            <div className="new-task" key={task.id}>
+          <div key={task.id}>
+            <div className="new-task">
               <div className="info-group">
-                <input type="checkbox" className="task-checkbox" />
+                <input type="checkbox" className="task-checkbox" onChange={() => setIsTaskCompleted(!task.isComplete)} />
                 <p>{task.name}</p>
               </div>
-              <button>
+              <button onClick={() => handleRemoveTask(task.id)}>
                 <img src={deleteImg} alt="Deletar task" />
               </button>
             </div>
             <hr />
-          </>
+          </div>
         ))}
       </main>
+
+      <ToastContainer />
     </Container>
   );
 }
